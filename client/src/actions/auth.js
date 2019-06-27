450626e5b690f596db6c68ee61a69bb9fddedf83
Ahmed Fathy <ahmedfathyoumara@gmail.com>
2019-06-27 07:28:16 +0200

    Logout & Navbar Links

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,7 +6,8 @@ import {
   USER_LOADED,
   AUTH_ERROR, 
   LOGIN_SUCCESS,
-  LOGIN_FAIL
+  LOGIN_FAIL,
+  LOGOUT
 } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
@@ -81,7 +82,7 @@ export const login = (email, password) => async dispatch => {
     });
 
     dispatch(loadUser());
-    
+
   } catch (err) {
     const errors = err.response.data.errors;
 
@@ -93,4 +94,10 @@ export const login = (email, password) => async dispatch => {
       type: LOGIN_FAIL
     });
   }
-}
+};
+
+// Logout / Clear Profile
+
+export const logout = () => dispatch => {
+  dispatch({ type: LOGOUT });
+};
